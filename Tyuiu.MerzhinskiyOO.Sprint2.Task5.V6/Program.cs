365a25a1d5c2f060390bd9ec4dadebf46fb10d01
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 05:20:54 +0600

    Task5

--- Tyuiu.MerzhinskiyOO.Sprint2.Task5.V6/Program.cs
+++ Tyuiu.MerzhinskiyOO.Sprint2.Task5.V6/Program.cs
@@ -7,6 +7,26 @@ namespace Tyuiu.MerzhinskiyOO.Sprint2.Task5.V6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("Введите номер масти: ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер достоинства карты: ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            var result = ds.FindCardNameAndValue(value1, value2);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
