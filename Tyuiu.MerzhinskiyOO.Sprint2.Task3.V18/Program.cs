04f62d0de0bad31a6fca4f08c7567e98ba9c013b
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 05:14:00 +0600

    Task3

--- Tyuiu.MerzhinskiyOO.Sprint2.Task3.V18/Program.cs
+++ Tyuiu.MerzhinskiyOO.Sprint2.Task3.V18/Program.cs
@@ -7,6 +7,17 @@ namespace Tyuiu.MerzhinskiyOO.Sprint2.Task3.V18
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Введите значение переменной X= ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadLine();
         }
     }
 }
