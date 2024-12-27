49c379b6fe15f487735d09509f789963aaf1d6e0
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 05:22:39 +0600

    Task6

---
+++ Tyuiu.MerzhinskiyOO.Sprint2.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint2.Task6.V4.Lib
+{
+    public class DataService : ISprint2Task6V4
+    {
+        public string FindCardSuit(int value)
+        {
+            switch (value)
+            {
+                case 1: return "Пики";
+                case 2: return "Трефы";
+                case 3: return "Бубны";
+                case 4: return "Червы";
+                default: return "Введено некорректное число";
+            }
+        }
+    }
+}
--- Tyuiu.MerzhinskiyOO.Sprint2.Task6.V4/Program.cs
+++ Tyuiu.MerzhinskiyOO.Sprint2.Task6.V4/Program.cs
@@ -7,6 +7,19 @@ namespace Tyuiu.MerzhinskiyOO.Sprint2.Task6.V4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите номер масти: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FindCardSuit(m));
+            Console.ReadKey();
         }
     }
 }
